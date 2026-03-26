b5dcc56f96834c353c646be954b3a02453b0291c
William Morriss <wjmelements@gmail.com>
2026-03-26 16:11:56 -0500

    lower default loglevel to info

--- src/logger.ts
+++ src/logger.ts
@@ -2,6 +2,6 @@ import { type Logger, pino } from 'pino'
 
 export function createLogger(config: { logLevel?: string | undefined }): Logger {
   return pino({
-    level: config.logLevel ?? 'error',
+    level: config.logLevel ?? 'info',
   })
 }
